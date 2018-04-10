39f138228c5248a0d63eb1b081c172a4a8ba7ced
Sam Fortunato <samfortunato@gmail.com>
2018-04-10 14:14:17 -0400

    Removed HideSeek. Coded search functionality.
    Removed HideSeek plugin.
    
    Implemented code that shows pictures with captions that match what users type in the search bar. It hides any pictures that don't match.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
 lightbox.option({
 	'alwaysShowNavOnTouchDevices': true
 });
+
+
+
+// Search
+
+const photos = document.getElementById('photo-gallery').children;
+
+document.body.addEventListener('keyup', (event) => {
+
+	event.preventDefault();
+
+	const searchValue = event.target.value.toLowerCase();
+
+	[...photos].forEach(photo => {
+
+		const photoCaption = photo.getAttribute('data-title').toLowerCase();
+
+		if (photoCaption.includes(searchValue)) {
+			photo.style.display = 'initial';
+		} else {
+			photo.style.display = 'none';
+		}
+
+	});
+
+});
